26481f4462e213b836e32b5531769c8f010acf72
SangHyeop <42771937+10150042@users.noreply.github.com>
2021-11-17 13:27:29 +0900

    Update ahnlab.py

--- ahnlab.py
+++ ahnlab.py
@@ -10,16 +10,16 @@ def ahnlab_parsing():
     context = soup.find_all(class_='posttitle')             # 기사 내 특정부분을 파싱하기 위해 전체 class 태그 내 값이 'posttitle' 인 태그 전체를 context 배열에 저장
     
     for i in range(5):
-        link = context[i].a.attrs['href']                                            # 추출한 값 내부에 하이퍼링크 url 값을 추출
-        title = context[i].text                                                      # 기사 제목 추출
-        title = title.replace("\n", "").replace("				","")# 기사 제목 마지막에 존재하는 공백 제거
-        title = title.replace("\"", "")                                              # 제목 내 '"'은 공백으로 치환
-        title = title.replace("\'", "")                                              # 제목 내 "'"은 공백으로 치환
-        info = ((link, title))                                                       # url, 제목을 묶어 info 변수에 저장
-        conn = sqlite3.connect('C:\\파싱\\db\\ahnlab.db', isolation_level=None)      # 특정 경로에 위치한 db파일을 연결
-        c = conn.cursor()                                                            # 커서 위치조정
-        c.execute("SELECT link FROM data WHERE link=(?)",(link,))                    # db내 해당 url이 존재하는지 질의
-        db_data = c.fetchone()                                                       # 조회한 결과값 반환
+        link = context[i].a.attrs['href']                                               # 추출한 값 내부에 하이퍼링크 url 값을 추출
+        title = context[i].text                                                         # 기사 제목 추출
+        title = title.replace("\n", "").replace("				","")   # 기사 제목 마지막에 존재하는 공백 제거
+        title = title.replace("\"", "")                                                 # 제목 내 '"'은 공백으로 치환
+        title = title.replace("\'", "")                                                 # 제목 내 "'"은 공백으로 치환
+        info = ((link, title))                                                          # url, 제목을 묶어 info 변수에 저장
+        conn = sqlite3.connect('C:\\파싱\\db\\ahnlab.db', isolation_level=None)         # 특정 경로에 위치한 db파일을 연결
+        c = conn.cursor()                                                               # 커서 위치조정
+        c.execute("SELECT link FROM data WHERE link=(?)",(link,))                       # db내 해당 url이 존재하는지 질의
+        db_data = c.fetchone()                                                          # 조회한 결과값 반환
         
         if db_data is None: # 결과값이 없을 시
             c.execute("INSERT INTO data VALUES(?,?)", info)                                       # db에 해당 info(url, 제목)를 추가
