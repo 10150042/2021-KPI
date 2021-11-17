0d87855e4692d195f31d0555a20fefbdf56c851e
SangHyeop <42771937+10150042@users.noreply.github.com>
2021-11-17 13:26:37 +0900

    Update ahnlab.py

--- ahnlab.py
+++ ahnlab.py
@@ -12,7 +12,7 @@ def ahnlab_parsing():
     for i in range(5):
         link = context[i].a.attrs['href']                                           # 추출한 값 내부에 하이퍼링크 url 값을 추출
         title = context[i].text                                                     # 기사 제목 추출
-        title = title.replace("\n", "").replace("				","")      # 기사 제목 마지막에 존재하는 공백 제거
+        title = title.replace("\n", "").replace("				","")# 기사 제목 마지막에 존재하는 공백 제거
         title = title.replace("\"", "")                                             # 제목 내 '"'은 공백으로 치환
         title = title.replace("\'", "")                                             # 제목 내 "'"은 공백으로 치환
         info = ((link, title))                                                      # url, 제목을 묶어 info 변수에 저장
