53fb5a841922c8c438f7e0680b2ae89a98fe557d
SangHyeop <42771937+10150042@users.noreply.github.com>
2021-11-17 12:31:57 +0900

    Create ahnlab.py

---
+++ ahnlab.py
@@ -0,0 +1,28 @@
+#-*-coding:utf-8-*-
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+
+def ahnlab_parsing():
+    url = 'https://asec.ahnlab.com/ko/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    context = soup.find_all(class_='posttitle')
+    
+    for i in range(5):
+        link = context[i].a.attrs['href']
+        title = context[i].text
+        title = title.replace("\n", "").replace("				","")
+        title = title.replace("\"", "")
+        title = title.replace("\'", "")
+        info = ((link, title))
+        conn = sqlite3.connect('C:\\파싱\\db\\ahnlab.db', isolation_level=None)
+        c = conn.cursor()
+        c.execute("SELECT link FROM data WHERE link=(?)",(link,))
+        db_data = c.fetchone()
+        
+        if db_data is None:
+            c.execute("INSERT INTO data VALUES(?,?)", info)
+            headers = {'Content-type': 'application/json; charset=utf-8'}
+            data = "{'text':" + "'" + "`안랩ASEC`" +" "+ title + '\n' + link + '\n' + "'}"
+            response = requests.post('https://hooks.slack.com/services/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', headers=headers, data=data.encode('utf-8'))
